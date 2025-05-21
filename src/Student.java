d807d3e7cc03cd27de37c2449af4209b40d46232
1427641 <1427641@sd61learn.ca>
2025-05-21 12:59:16 -0700

    add id to Student

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,46 @@
 public class Student {
-    int id;
+    private int id;
+    private int grade;
+    private Arraylist<Course> requestedChosenCourses = new Arraylist<>();
+    private Arraylist<Course> requestedAlternativeCourses = new Arraylist<>();
 
-    public Student(int id){
+
+    public Student(int id, int grade, Arraylist<Course> requestedChosenCourses, Arraylist<Course> requestedAlternativeCourses) {
         this.id = id;
-        System.out.println("Student created.");
+        this.grade = grade;
+        this.requestedChosenCourses = requestedChosenCourses;
+        this.requestedAlternativeCourses = requestedAlternativeCourses;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setRequestedChosenCourses(Arraylist<Course> requestedChosenCourses) {
+        this.requestedChosenCourses = requestedChosenCourses;
+    }
+
+    public Arraylist<Course> getRequestedChosenCourses() {
+        return requestedChosenCourses;
+    }
+
+    public void setRequestedAlternativeCourses(Arraylist<Course> requestedAlternativeCourses) {
+        this.requestedAlternativeCourses = requestedAlternativeCourses;
+    }
+
+    public Arraylist<Course> getRequestedAlternativeCourses() {
+        return requestedAlternativeCourses;
+    }
 }
